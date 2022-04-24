7db929b50c3af6c60ff5a59ae8cff2c13b8622c0
Leo Vivier <zaeph@zaeph.net>
2022-04-24 09:10:07 +0200

    Iteration

--- keyboards/moonlander/keymaps/zaeph/keymap.c
+++ keyboards/moonlander/keymaps/zaeph/keymap.c
@@ -41,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Layer: BASE
    *
    * .--------------------------------------------------.           .--------------------------------------------------.
-   * |    '   |   1  |   2  |   3  |   4  |   5  | MINS |           |  EQL |   6  |   7  |   8  |   9  |   0  |   +    |
+   * |    '   |   1  |   2  |   3  |   4  |   5  | MINS |           |  EQL |   6  |   7  |   8  |   9  |   0  |   '    |
    * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
    * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |  RCTL  |
    * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
-   * |LCTL/ESC|   A  |   S  |   D  |   F  |   G  |  (   |           |   )  |   H  |   J  |   K  |   L  |   ;  |   '    |
+   * |LCTL/ESC|   A  |   S  |   D  |   F  |   G  |  (   |           |   )  |   H  |   J  |   K  |   L  |   ;  |   ’    |
    * |--------+------+------+------+------+------|------´           `------|------+------+------+------+------+--------|
    * |  LSFT  |   Z  |   X  |   C  |   V  |   B  |                         |   N  |   M  |   ,  |   .  | LCTL |  RSFT  |
    * |--------+------+------+------+------+------´                         `------+------+------+------+------+--------|
-   * | ~META  | Cmd  | LAlt | RAlt |~META |  .-------------.     .-------------.  | RALT | DOWN |  UP  | RIGHT| ~META  |
+   * | ~META  | Cmd  | LAlt | RAlt | RALT |  .-------------.     .-------------.  | RALT | DOWN |  UP  | RIGHT| ~META  |
    *  `-----------------------------------´  |    HYPER    |     |    SUPER    |  `------------------------------------´
    *                                  .------+------+------|     |------+------+------.
    *                                  |      |      |      |     |      |      |      |
@@ -59,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [BASE] = LAYOUT_moonlander(
-                             KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PLUS,
+                             KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F24,
                              KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RCTL,
                              LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_LPRN, KC_RPRN, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
                              KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSPC,
-                             MO(META), KC_LGUI, KC_LALT, KC_RALT, MO(META), KC_RGUI, KC_LGUI, RALT_T(KC_LEFT), KC_DOWN, KC_UP, KC_RIGHT, MO(META),
+                             MO(META), KC_LGUI, KC_LALT, KC_RALT, KC_RALT, KC_RGUI, KC_LGUI, KC_RALT, KC_DOWN, KC_UP, KC_RIGHT, MO(META),
                              KC_BSPC, KC_LALT, KC_DEL, KC_LALT, KC_ENT, KC_SPC),
 
   /* Layer: META
