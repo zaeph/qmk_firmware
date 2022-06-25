fbdd21175cc2fb90c68fed2420fef276f2e9339e
Leo Vivier <zaeph@zaeph.net>
2022-06-25 14:59:23 +0200

    Revert to regular RSFT

--- keyboards/moonlander/keymaps/zaeph/keymap.c
+++ keyboards/moonlander/keymaps/zaeph/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F24,
                              KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL,
                              ZP_CESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL,  KC_UNDS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE,
-                             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,RSFT_T(KC_ENT),
+                             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,KC_RSFT,
                              MO(NUMB),KC_LGUI, KC_LALT, KC_RALT, MO(MOVE),KC_RGUI,                   KC_LGUI, KC_RALT, KC_DOWN, KC_UP,   KC_RIGHT,MO(SYMB),
                              KC_BSPC, MO(SYMB),KC_LALT,                                                                         KC_LALT, ZP_SYRET,KC_SPC),
 
