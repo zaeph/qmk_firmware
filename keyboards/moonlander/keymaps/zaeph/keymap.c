a37f0c7794202a3a4116c205a82e4efe0a8f23e8
Leo Vivier <zaeph@zaeph.net>
2022-06-25 17:05:49 +0200

    Improve symbol layer

--- keyboards/moonlander/keymaps/zaeph/keymap.c
+++ keyboards/moonlander/keymaps/zaeph/keymap.c
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-
 #include QMK_KEYBOARD_H
 #include "version.h"
 
@@ -27,20 +25,24 @@ enum layers {
   FUNC,  // functions
 };
 
-enum custom_keycodes {
-  VRSN = ML_SAFE_RANGE,
-  /* SET_RGB, */
-};
-
 #define BASE 0
 #define SYMB 1
 #define MOVE 2
 #define NUMB 3
 #define FUNC 4
 
-/* Custom keycodes */
+enum custom_keycodes {
+  VRSN = ML_SAFE_RANGE,
+  ZP_UPDIR = SAFE_RANGE,
+  /* SET_RGB, */
+};
+
+/* Aliases */
 #define ZP_CESC LCTL_T(KC_ESC)
-#define ZP_SYRET LT(SYMB, KC_ENT)
+/* #define ZP_SYRET LT(SYMB, KC_ENT) */
+#define ZP_RENT RSFT_T(KC_ENT)
+/* #define KC_COLN LSFT(KC_SCLN) */
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Layer: BASE
@@ -52,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
    * |LCTL/ESC|   A  |   S  |   D  |   F  |   G  | DEL  |           |   _  |   H  |   J  |   K  |   L  |   ;  |   ’    |
    * |--------+------+------+------+------+------|------´           `------|------+------+------+------+------+--------|
-   * |  LSFT  |   Z  |   X  |   C  |   V  |   B  |                         |   N  |   M  |   ,  |   .  |   /  |  RSFT  |
+   * |  LSFT  |   Z  |   X  |   C  |   V  |   B  |                         |   N  |   M  |   ,  |   .  |   /  |  RENT  |
    * |--------+------+------+------+------+------´                         `------+------+------+------+------+--------|
    * | ~NUMB  | CMD  | LALT | RALT |~MOVE |  .-------------.     .-------------.  | RALT | DOWN |  UP  | RIGHT| ~SYMB  |
    *  `-----------------------------------´  |    HYPER    |     |    SUPER    |  `------------------------------------´
    *                                  .------+------+------|     |------+------+------.
    *                                  |      |      |      |     |      |      |      |
-   *                                  | BSPC |~SYMB | LALT |     | LALT |~SYRET| SPC  |
+   *                                  | BSPC |~SYMB | LALT |     | LALT | ~SYMB| SPC  |
    *                                  |      |      |      |     |      |      |      |
    *                                  `--------------------´     `--------------------´
    */
@@ -67,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_MINS,    KC_EQL,     KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_F24,
                              KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LBRC,    KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_RCTL,
                              ZP_CESC,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_DEL,     KC_UNDS,    KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOTE,
-                             KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                               KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLASH,   KC_RSFT,
+                             KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                               KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLASH,   ZP_RENT,
                              MO(NUMB),   KC_LGUI,    KC_LALT,    KC_RALT,    MO(MOVE),   KC_RGUI,                            KC_LGUI,    KC_RALT,    KC_DOWN,    KC_UP,      KC_RIGHT,   MO(SYMB),
-                             KC_BSPC,    MO(SYMB),   KC_LALT,                                                                                                    KC_LALT,    ZP_SYRET,   KC_SPC),
+                             KC_BSPC,    MO(SYMB),   KC_LALT,                                                                                                    KC_LALT,    MO(SYMB),   KC_SPC),
 
 
   /* Layer: SYMB
@@ -77,11 +79,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * .--------------------------------------------------.           .--------------------------------------------------.
    * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
    * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-   * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+   * |        |  '   |  <   |  >   |  "   |  .   |      |           |      |   &  |   *  |   [  |   ]  |   %  |        |
    * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
-   * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+   * |        |  !   |  -   |  +   |  =   |  #   |      |           |      |   |  |   :  |   (  |   )  |   ?  |        |
    * |--------+------+------+------+------+------|------´           `------|------+------+------+------+------+--------|
-   * |        |      |      |      |      |      |                         |      |      |      |      |      |        |
+   * |        |  ^   |  /   |  *   |  \   | ../  |                         |   ~  |   $  |   {  |   }  |   @  |        |
    * |--------+------+------+------+------+------´                         `------+------+------+------+------+--------|
    * |        |      |      |      |      |  .-------------.     .-------------.  |      |      |      |      |        |
    *  `-----------------------------------´  |             |     |             |  `------------------------------------´
@@ -94,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [SYMB] = LAYOUT_moonlander(
                              _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
-                             _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LBRC,    KC_RBRC,    _______,    _______,    _______,
-                             _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LPRN,    KC_RPRN,    _______,    _______,    _______,
-                             _______,    _______,    _______,    _______,    _______,    _______,                            _______,    KC_LCBR,    KC_RCBR,    _______,    _______,    _______,
+                             _______,    KC_F24,     KC_LT,      KC_GT,      KC_DQT,     KC_DOT,     _______,    _______,    KC_AMPR,    KC_ASTR,    KC_LBRC,    KC_RBRC,    KC_PERC,    _______,
+                             _______,    KC_EXLM,    KC_MINS,    KC_PLUS,    KC_EQUAL,   KC_HASH,    _______,    _______,    KC_PIPE,    KC_COLN,    KC_LPRN,    KC_RPRN,    KC_QUES,    _______,
+                             _______,    KC_CIRC,    KC_SLSH,    KC_ASTR,    KC_BSLS,    ZP_UPDIR,                           KC_TILD,    KC_DLR,     KC_LCBR,    KC_RCBR,    KC_AT,      _______,
                              _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
                              _______,    _______,    _______,                                                                                                    _______,    _______,    _______),
 
@@ -153,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [NUMB] = LAYOUT_moonlander(
-                             _______,    _______,    _______,    _______,    _______,    RESET,      _______,    _______,    _______,    KC_7,       KC_8,       KC_9,       _______,    _______,
+                             _______,    _______,    _______,    _______,    _______,    _______,    RESET,      _______,    _______,    KC_7,       KC_8,       KC_9,       _______,    _______,
                              _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_4,       KC_5,       KC_6,       KC_PSCR,    _______,
                              _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_1,       KC_2,       KC_3,       _______,    _______,
                              MO(FUNC),   _______,    _______,    _______,    _______,    _______,                            _______,    KC_0,       KC_BSPC,    _______,    _______,    _______,
@@ -223,6 +225,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case VRSN:
       SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
       return false;
+
+    case ZP_UPDIR:
+      SEND_STRING ("../");
+      return false;
+
     /* case SET_RGB: */
     /*   if (record->event.pressed) { */
     /*     rgb_matrix_sethsv_noeeprom(18, 86, 95); */
