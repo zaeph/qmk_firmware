2b0ff98376c87a316ecec5c375df8481907f1d84
Leo Vivier <zaeph@zaeph.net>
2022-07-09 13:14:58 +0200

    De-mod BSPS and make Z the new LALT

--- keyboards/moonlander/keymaps/zaeph/keymap.c
+++ keyboards/moonlander/keymaps/zaeph/keymap.c
@@ -52,12 +52,13 @@ enum custom_keycodes {
 #define ZP_LCES LCTL_T(KC_ESC)
 #define ZP_SYRT LT(SYMB, KC_ENT)
 #define ZP_SYUD LT(SYMB, ZP_UNDS)
-#define ZP_BALT LALT_T(KC_BSPC)
+/* #define ZP_BALT LALT_T(KC_BSPC) */
 /* #define ZP_SALT LALT_T(KC_SPC) */
 #define ZP_LCCP LCTL_T(KC_RCTL)
 #define ZP_LCQT LCTL_T(KC_QUOTE)
 /* #define ZP_RASL RALT_T(KC_SLASH) */
 #define ZP_LASL LALT_T(KC_SLASH)
+#define ZP_LAKZ LALT_T(KC_Z)
 /* #define ZP_RCQT LCTL_T(KC_QUOTE) */
 /* #define ZP_RENT RSFT_T(KC_ENT) */
 
@@ -75,13 +76,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
    * |  LCES  |  A   |  S   |  D   |  F   |  G   | DEL  |           |   _  |   H  |   J  |   K  |   L  |   ;  |  LCQT  |
    * |--------+------+------+------+------+------+------´           `------+------+------+------+------+------+--------|
-   * |  LSFT  |  Z   |  X   |  C   |  V   |  B   |                         |   N  |   M  |   ,  |   .  | LASL |  RSFT  |
+   * |  LSFT  | LAKZ |  X   |  C   |  V   |  B   |                         |   N  |   M  |   ,  |   .  | LASL |  RSFT  |
    * |--------+------+------+------+------+------´                         `------+------+------+------+------+--------|
-   * | ~NUMB  | CMD  | LALT |      |~MOVE |  .-------------.     .-------------.  | RALT |   ↑  |   ↓  | RIGHT| ~SYMB  |
+   * | ~NUMB  | CMD  | LALT |      |~MOVE |  .-------------.     .-------------.  | RALT |      |      |      | ~SYMB  |
    *  `-----------------------------------´  |    HYPER    |     |    SUPER    |  `------------------------------------´
    *                                  .------+------+------|     |------+------+------.
    *                                  |      |      |      |     |      |      |      |
-   *                                  | BALT |~SYUD |      |     | LALT | ~SYRT|  SPC |
+   *                                  | BSPC |~SYUD |      |     | LALT | ~SYRT|  SPC |
    *                                  |      |      |      |     |      |      |      |
    *                                  `--------------------´     `--------------------´
    */
@@ -90,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_MINS,    KC_EQL,     KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       ZP_QUOT,
                              KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LBRC,    KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_RCTL,
                              ZP_LCES,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_DEL,     KC_UNDS,    KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    ZP_LCQT,
-                             KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                               KC_N,       KC_M,       KC_COMM,    KC_DOT,     ZP_LASL,    KC_RSFT,
+                             KC_LSFT,    ZP_LAKZ,    KC_X,       KC_C,       KC_V,       KC_B,                               KC_N,       KC_M,       KC_COMM,    KC_DOT,     ZP_LASL,    KC_RSFT,
                              MO(NUMB),   KC_LGUI,    KC_LALT,    KC_RALT,    MO(MOVE),   KC_RGUI,                            KC_LGUI,    KC_RALT,    KC_DOWN,    KC_UP,      KC_RIGHT,   MO(SYMB),
-                             ZP_BALT,    ZP_SYUD,    _______,                                                                                                    KC_LALT,    ZP_SYRT,    KC_SPC),
+                             KC_BSPC,    ZP_SYUD,    _______,                                                                                                    KC_LALT,    ZP_SYRT,    KC_SPC),
 
 
   /* Layer: SYMB
