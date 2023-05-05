95e7cbb529988221dbb15bc693ab5ab328bf5850
Leo Vivier <zaeph@zaeph.net>
2023-05-05 13:42:37 +0200

    Bump copyright

--- keyboards/moonlander/keymaps/zaeph/keymap.c
+++ keyboards/moonlander/keymaps/zaeph/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020-2022 Leo Vivier <zaeph@zaeph.net> (@zaeph)
+/* Copyright 2020-2023 Leo Vivier <zaeph@zaeph.net> (@zaeph)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
