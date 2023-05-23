264a61dd2b0f0e3eac8717a6cfab0030166a64f6
Leo Vivier <zaeph@zaeph.net>
2023-05-23 10:58:47 +0200

    Add copyright and bump GPL

--- keyboards/planck/keymaps/zaeph/keymap.c
+++ keyboards/planck/keymaps/zaeph/keymap.c
@@ -1,8 +1,9 @@
 /* Copyright 2015-2017 Jack Humbert
+ * Copyright 2021-2023 Leo Vivier
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
